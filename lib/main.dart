9abad426675a458138096abc3fefced4f257f118
FlutLab.io <>
2022-05-08 13:51:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.white,
+        backgroundColor: Theme.of(context).primaryColorDark,
       ),
       body: pages[pageIndex],
       bottomNavigationBar: buildMyNavBar(context),
