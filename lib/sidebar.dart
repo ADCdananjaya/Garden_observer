fa718720932aa8f8d0e0204db69f718eff561b1d
FlutLab.io <>
2022-05-08 15:45:06 +0000

    Autosave

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -8,13 +8,15 @@ class SideBar extends StatelessWidget {
       child: ListView(
         children: [
           UserAccountsDrawerHeader(
-            accountName: Text('FlutLab User'),
-            accountEmail: Text('flutlab.ide@gmail.com'),
+            accountName: Text('Your name'),
+            accountEmail: Text('gardenobserver@gmail.com'),
             currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Text(
-                "F",
-                style: TextStyle(fontSize: 40.0),
+              child: ClipOval(
+                child: Image.network(
+                  'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_960_720.png',
+                  width: 90,
+                  height: 90,
+                ),
               ),
             ),
           )
