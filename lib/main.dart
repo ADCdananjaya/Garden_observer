ac3b6c1557de5df9dd0cea5eae69adfde35817ba
FlutLab.io <>
2022-05-08 09:33:44 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,53 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyNavigationBar(),
+      title: 'Garden Observer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Home'),
     );
   }
 }
 
-class MyNavigationBar extends StatefulWidget {
-  MyNavigationBar({Key key}) : super(key: key);
-
-  @override
-  _MyNavigationBarState createState() => _MyNavigationBarState();
-}
-
-class _MyNavigationBarState extends State<MyNavigationBar> {
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Search Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Profile Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter BottomNavigationBar Example'), backgroundColor: Colors.green),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home'), backgroundColor: Colors.green),
-        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search'), backgroundColor: Colors.yellow),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          title: Text('Profile'),
-          backgroundColor: Colors.blue,
-        ),
-      ], type: BottomNavigationBarType.shifting, currentIndex: _selectedIndex, selectedItemColor: Colors.black, iconSize: 40, onTap: _onItemTapped, elevation: 5),
+      body: Text('Home'),
     );
   }
 }
