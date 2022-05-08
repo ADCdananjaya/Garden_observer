0c3f4ff8792328614a4833ed0217de0da3e3ffb1
FlutLab.io <>
2022-05-08 15:39:12 +0000

    Autosave

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -4,6 +4,22 @@ class SideBar extends StatelessWidget {
   const SideBar({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('FlutLab User'),
+            accountEmail: Text('flutlab.ide@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Text(
+                "F",
+                style: TextStyle(fontSize: 40.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
