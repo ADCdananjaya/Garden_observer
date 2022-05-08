53b656f9987bb4aaf56d0ded37a39bfee44b8a1b
FlutLab.io <>
2022-05-08 13:07:01 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -43,8 +43,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: const Color(0xffC4DFCB),
-      backgroundColor: const Color.fromRGBO(35, 133, 175, 2),
+      backgroundColor: const Color(0xffC4DFCB),
       appBar: AppBar(
         leading: Icon(
           Icons.menu,
