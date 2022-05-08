1e0eee60bf0236d1ecb736c49e7b0bff959b6ffd
FlutLab.io <>
2022-05-08 13:47:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _HomePageState extends State<HomePage> {
     return Container(
       height: 60,
       decoration: BoxDecoration(
-        color: Theme.of(context).focusColor,
+        color: Theme.of(context).primaryColorLight,
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(20),
           topRight: Radius.circular(20),
