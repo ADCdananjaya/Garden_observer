17b0bae563d0421268988d4f7323dfe3db82e1b0
FlutLab.io <>
2022-05-08 15:55:38 +0000

    Autosave

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -19,6 +19,13 @@ class SideBar extends StatelessWidget {
                 ),
               ),
             ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://images.pexels.com/photos/409696/pexels-photo-409696.jpeg',
+                ),
+              ),
+            ),
           )
         ],
       ),
