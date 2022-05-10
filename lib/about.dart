b7254c37f479287c5bf2a49e05879096dfacdab3
FlutLab.io <>
2022-05-10 20:53:52 +0000

    Autosave

--- lib/about.dart
+++ lib/about.dart
@@ -4,6 +4,12 @@ class AboutUs extends StatelessWidget {
   const AboutUs({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Widget buidImage() => Image.network(
+          'https://cdn3.iconfinder.com/data/icons/agriculture-farming-and-gardening-vol-6/90/Save_Plantsc7-1024.png',
+          fit: BoxFit.cover,
+          width: 100,
+          height: 100,
+        );
     return Scaffold(
       appBar: AppBar(
         title: Text('About'),
@@ -15,15 +21,16 @@ class AboutUs extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
             side: BorderSide(color: Colors.black, width: 2),
           ),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                buidImage(),
+              ],
+            ),
+          ),
         ),
       ),
     );
-
-    Widget buidImage() => Image.network(
-          'https://cdn3.iconfinder.com/data/icons/agriculture-farming-and-gardening-vol-6/90/Save_Plantsc7-1024.png',
-          fit: BoxFit.cover,
-          width: 100,
-          height: 100,
-        );
   }
 }
