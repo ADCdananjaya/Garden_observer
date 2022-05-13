ce1dec5cb8aba3a595407d1ad87454c26f9b2e94
FlutLab.io <>
2022-05-13 17:04:44 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -9,21 +9,10 @@ class Page1 extends StatelessWidget {
   static const supportingText2 = 'NPK levels';
 
   Card buildCard(var cardImage, var supportingText) {
-    //var heading = '\$2300 per month';
-    //var subheading = '2 bed, 1 bath, 1300 sqft';
-    //var cardImage = NetworkImage('https://source.unsplash.com/random/800x600?house');
-    //var supportingText = 'Beautiful home to rent, recently refurbished with modern appliances...';
     return Card(
         elevation: 4.0,
         child: Column(
           children: [
-            /*
-            ListTile(
-              title: Text(heading),
-              subtitle: Text(subheading),
-              trailing: Icon(Icons.favorite_outline),
-            ),
-            */
             Container(
               height: 150.0,
               child: Ink.image(
@@ -43,24 +32,6 @@ class Page1 extends StatelessWidget {
                 ),
               ),
             ),
-            /*
-            ButtonBar(
-              children: [
-                TextButton(
-                  child: const Text('CONTACT AGENT'),
-                  onPressed: () {
-                   
-                  },
-                ),
-                TextButton(
-                  child: const Text('LEARN MORE'),
-                  onPressed: () {
-                    
-                  },
-                )
-              ],
-            )
-            */
           ],
         ));
   }
@@ -70,19 +41,6 @@ class Page1 extends StatelessWidget {
     return Container(
       color: const Color(0xffF9F9F9),
       padding: EdgeInsets.only(left: 10.0, right: 10.0),
-      /*
-      child: Center(
-        child: Text(
-          "Home Page",
-          style: TextStyle(
-            color: Colors.greenAccent[900],
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
-            //backgroundColor: Colors.blue,
-          ),
-        ),
-      ),
-      */
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
