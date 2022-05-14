a4bd61c7435a523d06585ffe20a86b17fcb68cb0
FlutLab.io <>
2022-05-14 20:52:19 +0000

    Autosave

--- lib/staff.dart
+++ lib/staff.dart
@@ -7,6 +7,7 @@ class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xffF9F9F9),
+      /*
       child: Center(
         child: Text(
           "Staff Page",
@@ -16,6 +17,38 @@ class Page2 extends StatelessWidget {
             fontWeight: FontWeight.w500,
           ),
         ),
+      ),
+      */
+      child: Column(
+        children: [
+          Container(
+            height: 230,
+            child: Stack(
+              children: [
+                Positioned(
+                  child: Material(
+                    child: Container(
+                      height: 180.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          new BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            offset: new Offset(-10.0, 10.0),
+                            blurRadius: 20.0,
+                            spreadRadius: 4.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
