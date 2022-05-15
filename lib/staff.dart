d377215bede60a4f754b19ab143655d7feae1251
FlutLab.io <>
2022-05-15 16:20:47 +0000

    Autosave

--- lib/staff.dart
+++ lib/staff.dart
@@ -65,7 +65,7 @@ class Page2 extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                           image: DecorationImage(
                             fit: BoxFit.fill,
-                            image: AssetImage('chamod.jpg'),
+                            image: AssetImage('assets/chamod.jpg'),
                             //image: NetworkImage('https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png'),
                           ),
                         ),
