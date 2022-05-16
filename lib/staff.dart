d636f058f03b1647a7a96a060f97d1e2ea36a1d3
FlutLab.io <>
2022-05-16 13:27:41 +0000

    Autosave

--- lib/staff.dart
+++ lib/staff.dart
@@ -142,7 +142,7 @@ class Page2 extends StatelessWidget {
                               ),
                               IconButton(
                                 iconSize: 40,
-                                icon: changeColor(Icons.favorite, Colors.blue),
+                                icon: changeColor(Icons.facebook, Colors.blue),
                                 onPressed: () => changeColor(Icons.favorite, Colors.red),
                               ),
                             ],
