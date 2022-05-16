28c5eec3397b797126c2619a0ce4a425b3dde138
FlutLab.io <>
2022-05-16 11:54:17 +0000

    Autosave

--- lib/staff.dart
+++ lib/staff.dart
@@ -90,6 +90,7 @@ class Page2 extends StatelessWidget {
                       height: 150,
                       width: 180,
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Text(
