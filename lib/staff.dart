90381179314d5dfac1d756c7abeca8926fc472eb
FlutLab.io <>
2022-05-16 12:14:57 +0000

    Autosave

--- lib/staff.dart
+++ lib/staff.dart
@@ -120,13 +120,11 @@ class Page2 extends StatelessWidget {
                           ),
                           Row(
                             children: [
-                              /*
                               IconButton(
                                 //iconSize: 20,
                                 icon: Icon(Icons.mail),
                                 onPressed: () => print('button clicked'),
                               ),
-                              */
                             ],
                           ),
                         ],
