771d933dafe455226f55eee7b731b1fa0c47b63d
FlutLab.io <>
2022-05-17 21:12:35 +0000

    Autosave

--- lib/chat.dart
+++ lib/chat.dart
@@ -8,6 +8,7 @@ class Page3 extends StatelessWidget {
     return Container(
       color: const Color(0xffF9F9F9),
       child: Center(
+        /*
         child: Text(
           "Chat Page",
           style: TextStyle(
@@ -15,6 +16,20 @@ class Page3 extends StatelessWidget {
             fontSize: 45,
             fontWeight: FontWeight.w500,
           ),
+        ),
+        */
+        child: Column(
+          children: [
+            Text(
+              "Chat Page",
+              style: TextStyle(
+                color: Colors.greenAccent[900],
+                fontSize: 45,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Image.network('https://cdn.pixabay.com/photo/2017/06/20/08/12/maintenance-2422173_960_720.png'),
+          ],
         ),
       ),
     );
